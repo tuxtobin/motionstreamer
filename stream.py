446d980ae3f8b2a53e77e760a88605267006249b
gallops <sam.gallops@nbi.ac.uk>
2020-05-31 15:30:44 +0100

    initial commit with main function and arguments

---
+++ stream.py
@@ -0,0 +1,11 @@
+import argparse
+
+
+if __name__ == '__main__':
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-i", "--ip", type=str, default="127.0.0.1", required=False, help="IP address of server")
+    ap.add_argument("-p", "--port", type=int, default=8888, required=False, help="Port of server")
+    ap.add_argument("-r", "--rotate", type=int, default=0, required=False, help="Rotate image")
+    ap.add_argument("-e", "--edges", action="store_true", default=False, required=False,
+                    help="Turn on edge detection")
+    args = vars(ap.parse_args())
