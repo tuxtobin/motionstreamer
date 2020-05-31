e425af8d274c6d8d32e6bc99991e27788d14921c
gallops <sam.gallops@nbi.ac.uk>
2020-05-31 15:42:16 +0100

    include version information

--- stream.py
+++ stream.py
@@ -1,6 +1,9 @@
 import argparse
 
 
+__version__ = 0.1
+
+
 if __name__ == '__main__':
     ap = argparse.ArgumentParser()
     ap.add_argument("-i", "--ip", type=str, default="127.0.0.1", required=False, help="IP address of server")
@@ -8,4 +11,6 @@ if __name__ == '__main__':
     ap.add_argument("-r", "--rotate", type=int, default=0, required=False, help="Rotate image")
     ap.add_argument("-e", "--edges", action="store_true", default=False, required=False,
                     help="Turn on edge detection")
+    ap.add_argument("-v", "--version", action="version",
+                    version="%(prog)s {version}".format(version=__version__))
     args = vars(ap.parse_args())
