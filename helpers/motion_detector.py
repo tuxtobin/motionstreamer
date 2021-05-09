948b747fee22166e935f741dc1cf19fb2461b901
tuxtobin <tobingallop@yahoo.co.uk>
2021-05-09 17:25:54 +0100

    add hidden area to block frequency moving areas (e.g. tree tops, plants)

--- helpers/motion_detector.py
+++ helpers/motion_detector.py
@@ -4,8 +4,12 @@ import cv2
 
 
 class MotionDetector:
-    def __init__(self, accum_weight=0.5):
+    def __init__(self, accum_weight=0.5, x1=0, y1=0, x2=640, y2=480):
         self.accum_weight = accum_weight
+        self.x1 = x1
+        self.y1 = y1
+        self.x2 = x2
+        self.y2 = y2
         self.bg = None
 
     def update(self, image):
@@ -27,12 +31,15 @@ class MotionDetector:
         (minX, minY) = (np.inf, np.inf)
         (maxX, maxY) = (-np.inf, -np.inf)
 
-        if len(cnts) == 0:
-            return None
-
+        bound = False
         for c in cnts:
             (x, y, w, h) = cv2.boundingRect(c)
-            (minX, minY) = (min(minX, x), min(minY, y))
-            (maxX, maxY) = (max(maxX, x + w), max(maxY, y + h))
+            if self.x1 <= x >= self.x2 and self.y2 <= y >= self.y2:
+                (minX, minY) = (min(minX, x), min(minY, y))
+                (maxX, maxY) = (max(maxX, x + w), max(maxY, y + h))
+                bound = True
 
-        return thresh, (minX, minY, maxX, maxY)
+        if bound:
+            return thresh, (minX, minY, maxX, maxY)
+        else:
+            return None
